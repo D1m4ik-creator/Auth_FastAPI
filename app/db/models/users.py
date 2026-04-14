e4d33521b2f9594e9852554104335ae6dff32694
Dim4ik-creator <litvinov06062006@gmail.com>
2026-04-14 13:58:10 +0300

    added changes for model

--- app/db/models/users.py
+++ app/db/models/users.py
@@ -1,1 +1,21 @@
+import uuid
+from enum import Enum
 
+from sqlalchemy import Column, String, DateTime, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
---
+++ app/db/schema/users.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, field_validator, model_validator, EmailStr
+import re
+
+
+PASSWORD_RE = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,64}$")
+
+
+class UserCreate(BaseModel):
+    name: str = Field(..., example="John Doe")
+    email: EmailStr = Field(..., example="john.doe@example.com")
+    password: str = Field(..., example="securepassword")
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if not PASSWORD_RE.match(value):
+            raise ValueError("Пароль должен быть от 8 до 64 символов и содержать хотя бы одну заглавную букву, одну строчную букву, одну цифру и один специальный символ.")
+        return value
+    
